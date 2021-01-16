09ebdbecc62ba9de34177e074340f6947d704920
Ammar <ammarraneez@gmail.com>
2021-01-16 19:12:05 +0530

    prediction works. Nice

--- Mark Prediction With Flask/app.py
+++ Mark Prediction With Flask/app.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template, redirect, request
+import joblib
+import math
 
 #__name__ = __main__
 app = Flask(__name__)
+model = joblib.load("model.pkl")
 
 @app.route("/")
 def hello():
     return render_template("index.html")
+
+@app.route("/", methods = ['POST'])
+def marks():
+    if request.method == 'POST':
+        hours = float(request.form['hours'])
+        prediction = math.floor(model.predict([[hours]])[0][0])
+        marks = str(prediction)
+
+    return render_template("index.html", your_marks = marks)
 
 if __name__ == '__main__':
     app.run(debug = True)
