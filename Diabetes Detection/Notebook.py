2afa3982e84e34d6d27ba3630f85ce811e1d5fa8
Ammar <ammarraneez@gmail.com>
2021-01-18 02:04:29 +0530

    added confusion matrix

--- Diabetes Detection/Notebook.py
+++ Diabetes Detection/Notebook.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from PIL import Image
 import streamlit as st
-import joblib
+import math
 
 #Main title and heading
 st.write("""
@@ -40,14 +40,14 @@ X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.25, rand
 
 #Get all user input
 def get_user_input():
-    pregnancies = st.slider('pregnancies', 0, 17, 3)
-    blood_glucose = st.slider('blood_glucose', 0, 199, 117)
-    blood_pressure = st.slider('blood_pressure', 0, 122, 72)
-    skin_thickness = st.slider('skin_thickness', 0, 99, 23)
-    insulin = st.slider('insulin', 0.0, 846.0, 30.0)
-    BMI = st.slider('BMI', 0.0, 67.1, 32.0)
-    DPF = st.slider('DPF', 0.078, 2.42, 0.3725)
-    Age = st.slider('Age', 21, 81, 29)
+    pregnancies = st.sidebar.slider('pregnancies', 0, 17, 3)
+    blood_glucose = st.sidebar.slider('blood_glucose', 0, 199, 117)
+    blood_pressure = st.sidebar.slider('blood_pressure', 0, 122, 72)
+    skin_thickness = st.sidebar.slider('skin_thickness', 0, 99, 23)
+    insulin = st.sidebar.slider('insulin', 0.0, 846.0, 30.0)
+    BMI = st.sidebar.slider('BMI', 0.0, 67.1, 32.0)
+    DPF = st.sidebar.slider('DPF', 0.078, 2.42, 0.3725)
+    Age = st.sidebar.slider('Age', 21, 81, 29)
         
     #store all input
     user_inputs = {
@@ -77,7 +77,10 @@ randomForestClassifier.fit(X_train, Y_train)
 
 #Show model metrics
 st.subheader("Model Test Accuracy Score: ")
-st.write(str(accuracy_score(Y_test, randomForestClassifier.predict(X_test)) * 100) + "%")
+st.write(str(math.floor(accuracy_score(Y_test, randomForestClassifier.predict(X_test)) * 100)) + "%")
+st.subheader("Model Confusion Matrix: ")
+st.write(confusion_matrix(Y_test, randomForestClassifier.predict(X_test)))
+
 
 #Store model predictions
 prediction = randomForestClassifier.predict(input_data)
